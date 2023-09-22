42db7f664b0ad530784a30c28f0db3d946fc0dbe
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2023-09-23 09:37:46 +1200

    sidebar opt

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,16 +2,7 @@ import React from "react";
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Image from "next/image";
 import { logo } from "@/public/images/util";
 
